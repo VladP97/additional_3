e1456c604e0ced50f9e877ff859e0fa965da8671
VladP97 <vlad.potapov1997@gmail.com>
2017-02-21 22:37:26 +0300

    additional_3/fix2

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
 module.exports = function getBase(base) {
   var copyBase;
-  var reminder;
-  var count;
-  var j;
   for (let i = 2; i < base; i++) {
     copyBase = +base;
-    reminder = [];
-    count = 0;
-    j = 0;
     while (copyBase > i ) {
-      reminder[j] = copyBase % i;
-      copyBase = Math.floor(copyBase / i);
-      j++;
-    }
-    for (let ii = 0; ii < reminder.length; ii++) {
-      if(reminder[ii] == 1){
-        count++;
+      if(copyBase % i != 1){
+        break;
       }
+      copyBase = Math.floor(copyBase / i);
     }
-    if(reminder.length == count){
-      return i.toString();
+    if (copyBase <= i) {
+      return i.toFixed();
     }
   }
 }
