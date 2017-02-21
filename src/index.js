dda0eb93e8d07e601fac29ba6d7cf841649a2b52
VladP97 <vlad.potapov1997@gmail.com>
2017-02-21 21:29:33 +0300

    additional_3/feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function getBase(base) {
-  // your solution
+  var copyBase;
+  var reminder;
+  var count;
+  var j;
+  for (let i = 2; i < base; i++) {
+    copyBase = +base;
+    reminder = [];
+    count = 0;
+    j = 0;
+    while (copyBase > i ) {
+      reminder[j] = copyBase % i;
+      copyBase = Math.floor(copyBase / i);
+      j++;
+    }
+    for (let ii = 0; ii < reminder.length; ii++) {
+      if(reminder[ii] == 1){
+        count++;
+      }
+    }
+    if(reminder.length == count){
+      return i;
+    }
+  }
 }
